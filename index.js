361e2334e47a48b586396d80d9a821470891ffd1
shashlad7 <shashankkr@ganakalabs.com>
2023-12-17 23:44:11 +0530

    set interval uncommented

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function randomInterval() {
   return Math.floor(Math.random() * (120000 - 45000 + 1)) + 45000;
 }
 
-// setInterval(() => {
-autoReply(oAuth2Client);
-// }, randomInterval());
+setInterval(() => {
+  autoReply(oAuth2Client);
+}, randomInterval());
 
 app.listen(PORT, () => {
   console.log("STEP0 : [ SERVER STARTED ON PORT : " + PORT + " ✅ ]");
